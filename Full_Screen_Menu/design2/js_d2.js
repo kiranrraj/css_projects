5d132363afe8fc0b52eb820f9798cc83c5dd9ef0
Kiran Raj R <kiranrajvjd@gmail.com>
2021-06-04 15:31:49 +0530

    CSS Components : Full Screen Menu

--- Full_Screen_Menu/design2/js_d2.js
+++ Full_Screen_Menu/design2/js_d2.js
@@ -3,8 +3,7 @@ let side2 = document.querySelector('.side2');
 let overlay = document.querySelector('.menu_overlay');
 let open = document.querySelector('.fa-bars');
 let close = document.querySelector('.fa-times');
-
-console.log(side1, side2, overlay, open, close);
+let list = document.querySelectorAll('.item');
 
 open.addEventListener('click', ()=>{
     overlay.style.transform = "translateX(0)";
@@ -14,4 +13,16 @@ open.addEventListener('click', ()=>{
 close.addEventListener('click', ()=>{
     overlay.style.transform = "translateX(110%)";
     side2.style.transform = "translateX(-110%)";
+});
+
+list.forEach(elem =>{
+    elem.addEventListener('mouseover', ()=>{
+        let anchor = elem.getElementsByTagName('a')[0];
+        let anchorColor = window.getComputedStyle(anchor).getPropertyValue('color');
+        console.log(anchor, anchorColor);
+        side2.style.background = `linear-gradient(to left, #4b134f, ${anchorColor})`;
+    });
+    elem.addEventListener('mouseleave', ()=>{
+        side2.style.background = `linear-gradient(to left, #4b134f, #c94b4b`;
+    })
 })
--- Full_Screen_Menu/design3/js_d3.js
+++ Full_Screen_Menu/design3/js_d3.js
@@ -1,18 +1,20 @@
 let side1 = document.querySelector('.side1');
 let side2 = document.querySelector('.side2');
+let list = document.querySelector('.list');
 let overlay = document.querySelector('.menu_overlay');
 let open = document.querySelector('.fa-bars');
 let close = document.querySelector('.fa-times');
 
-console.log(side1, side2, overlay, open, close);
 
 open.addEventListener('click', ()=>{
+    list.style.left = "-50%";
     overlay.style.transform = "translateY(0)";
     side2.style.transform = "translateX(0%)";
     side1.style.transform = "translateY(0%)";
 })
 
 close.addEventListener('click', ()=>{
+    list.style.left = "200%";
     overlay.style.transform = "translateY(-110%)";
     side1.style.transform = "translateY(100%)";
     side2.style.transform = "translateX(-100%)";
