3d536698258cf75134e907b65cde8c3b918b2f03
Kiran Raj R <kiranrajvjd@gmail.com>
2021-08-03 12:48:21 +0530

    Project_17: Update 3

---
+++ website_Projects/project_17_portfolio/scripts/main_js.js
@@ -0,0 +1,11 @@
+const closeBtn = document.querySelector('.header--close i');
+const toggleBtn = document.querySelector('.header--toggle i');
+const nav = document.querySelector('.header--nav');
+
+closeBtn.addEventListener('click', ()=>{
+    nav.classList.remove('toggle');
+});
+
+toggleBtn.addEventListener('click', ()=>{
+    nav.classList.toggle('toggle');
+});
