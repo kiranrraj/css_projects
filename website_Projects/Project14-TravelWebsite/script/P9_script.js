5d8aed50ef561ddbf16c47d275ec54564eddf2ef
Kiran Raj R <kiranrajvjd@gmail.com>
2021-08-04 22:37:42 +0530

    Project14: Update 2

--- website_Projects/Project14-TravelWebsite/script/P9_script.js
+++ website_Projects/Project14-TravelWebsite/script/P9_script.js
@@ -1,3 +1,4 @@
+const home = document.querySelector('.home');
 const headerContainer = document.querySelector('.header--navBar');
 const iconContainer = document.querySelector('.header--hamburgerIcon');
 const icon = document.querySelector('.header--hamburgerIcon i');
@@ -7,4 +8,5 @@ iconContainer.addEventListener('click', ()=>{
     headerContainer.classList.toggle('header--active');
     icon.classList.toggle('fa-bars');
     icon.classList.toggle('fa-times');
+    home.classList.toggle('homeSlide');
 })
